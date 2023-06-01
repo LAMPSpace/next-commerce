a9ab7980085206e3e90bd39bd54ed6d221cbe03a
Tang Hien Phan <phan05042000@gmail.com>
2023-06-01 16:03:00 +0700

    Upload new images

--- storefront/asset/logos/index.ts
+++ storefront/asset/logos/index.ts
@@ -8,6 +8,12 @@ import logo_lg from './logo_lg.png';
 import logo_msi from './logo_msi.png';
 import logo_oppo from './logo_oppo.png';
 import logo_vivo from './logo_vivo.png';
+import logo_hp from './logo_hp.png';
+import logo_jbl from './logo_jbl.png';
+import logo_realme from './logo_realme.png';
+import logo_asus from './logo_asus.png';
+import logo_acer from './logo_acer.png';
+import logo_lenovo from './logo_lenovo.png';
 
 export type ListLogos = {
     [key: string]: StaticImageData;
@@ -22,7 +28,13 @@ const listLogos: ListLogos = {
     logo_lg,
     logo_msi,
     logo_oppo,
-    logo_vivo
+    logo_vivo,
+    logo_realme,
+    logo_hp,
+    logo_jbl,
+    logo_acer,
+    logo_asus,
+    logo_lenovo
 };
 
 export default listLogos;
